d3c7566954b2772671fe344d5b93c18221535c7c
d-ayhan <41747300+d-ayhan@users.noreply.github.com>
2018-08-10 17:57:05 +0000

    Add files via upload

---
+++ FASTAeditWithVCF.m
@@ -0,0 +1,39 @@
+function FASTAeditWithVCF(VCF,FASTA,OUTFASTA)
+% This script edits given FASTA file with the variants in the VCF file.
+% VCF has to be sorted by coordinates.
+% if OUTFASTA exist, the script will expand the file. Delete the previously
+% exising file before running.
+% % Example command line
+% FASTAeditWithVCF('v19.WT_S11.fb.vcf','4287-WT.v19.fasta','new.fasta')
+% Dilay Ayhan, 2018
+
+formatSpec = '%s%n%q%s%s%n%q%q%q%q%[^\n]';
+fileID = fopen(VCF,'r');
+dataArray = textscan(fileID,formatSpec,'Delimiter', '\t', ...
+    'TextType', 'string','CommentStyle','#',...
+    'ReturnOnError', false, 'EndOfLine', '\n');
+fclose(fileID);
+
+FA=fastaread(FASTA);
+newFA=FA;
+FAheaders={FA.Header}';
+
+variants = {flipud(dataArray{1}), flipud(dataArray{2}), flipud(dataArray{4}),...
+    flipud(dataArray{5})}; % reverse order for fixing without causing problems
+
+[l,p] = ismember(variants{1},FAheaders); % find entries
+if ~min(l) % if l has 0
+    disp('VCF has following CHROM entries that are not present in the FASTA');
+    disp(unique(dataArray{1}(~l)))
+    return
+end
+
+for i=1:length(variants{1,1})
+    newFA(p(i)).Sequence = replaceBetween(FA(p(i)).Sequence,...
+        variants{1,2}(i),variants{1,2}(i)+strlength(variants{1,3}(i))-1,...
+        variants{1,4}(i));
+end
+
+myfastawrite(OUTFASTA,newFA);
+
+end
